758182fb1c5c69c6c64af9c8c867dc60ff8a9f0d
Ádám Sárpátki <sepa1989@gmail.com>
2021-09-14 14:31:14 +0200

    add mockup of mobile design of the case study card

--- src/Index.vue
+++ src/Index.vue
@@ -9,12 +9,34 @@
   </div>
 
   <!-- Case Studies -->
-  <div class="desktop:h-496 md:h-496 sm:h-470 px-4 desktop:px-32 md:px-6 sm:px-4 flex flex-row items-end justify-center">
+<!--   <div class="desktop:h-496 md:h-496 sm:h-470 px-4 desktop:px-32 md:px-6 sm:px-4 flex flex-row items-end justify-center">
     <div class="desktop:pl-8 md:pl-4 sm:pl-4 desktop:pt-245 md:pt-124 md:pr-80 sm:pt-162 sm:pr-276 h-456 desktop:w-984 md:w-720 sm:w-608 flex flex-col items-start text-left bg-rulebook-card-small desktop:bg-rulebook-card-desktop md:bg-rulebook-card-tablet sm:bg-rulebook-card-mobile filter bg-cover drop-shadow-card">
       <router-link to="/rulebook"><h2 class="display-medium-smaller text-gray-900">D&D Rulebook</h2></router-link>
       <p class="subheading-italic text-gray-900">A mobile-based solution for the players of Dungeons & Dragons.</p>
     </div>
+  </div> -->
+
+  <div class="relative filter drop-shadow-card flex flex-row items-end justify-center md:h-496 sm:h-470 px-4 desktop:px-32 md:px-6 sm:px-4 h-456">
+      <!-- <div class="bg-gray-900 md:h-496 sm:h-470 h-456 w-343 absolute opacity-70"></div> -->
+      <picture class="filter blur sm:filter-none">
+        <source media="(max-width: 375px" srcset="/src/assets/study-card-small@2x.png">
+        <source media="(min-width: 1440px" srcset="/src/assets/study-card-desktop@2x.png">
+        <!-- <source media="(max-width: 767px" srcset="/src/assets/rulebook-card-mobile.png"> -->
+        <source media="(max-width: 768px" srcset="/src/assets/study-card-tablet@2x.png">
+        <source media="(max-width: 641px" srcset="/src/assets/study-card-small@2x.png">
+        <source media="(max-width: 376px" srcset="/src/assets/study-card-mobile@2x.png">
+        <!-- <source media="(max-width: 1439px" srcset="/src/assets/rulebook-card-tablet.png"> -->
+        <img class="h-456" src="/src/assets/study-card-desktop@2x.png" alt="The header image of the D&D Rulebook project">
+      </picture>
+
+    <div class="absolute desktop:pl-8 md:pl-4 sm:pl-4 desktop:pt-245 md:pt-124 md:pr-80 sm:pt-162 sm:pr-276 h-456 desktop:w-984 md:w-720 sm:w-608 w-343 sm:text-left flex flex-col justify-center">
+      <h2 class="display-medium-smaller text-gray-900">D&D Rulebook</h2>
+      <p class="subheading-italic text-gray-900">
+        A mobile-based solution for the players of Dungeons & Dragons.
+      </p>
+    </div>
   </div>
+
 
   <WorkTogether/>
 </template>
