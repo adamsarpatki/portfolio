070e33aee830de18aaf26051ab85ecb57ce0405d
Ádám Sárpátki <sepa1989@gmail.com>
2022-03-30 17:46:32 +0200

    add conclusion chapter to gabo case study

--- src/components/GaboConclusion.vue
+++ src/components/GaboConclusion.vue
@@ -4,28 +4,17 @@
   </div>
 
   <div class="study-grid h-auto">
-    <div class="desktop:col-start-1 desktop:col-end-7 col-span-full text-left">
+    <div class="desktop:col-start-1 desktop:col-end-13 col-span-full text-left">
       <h3 class="h3-bold text-gray-900 mb-7">What I’ve Learned</h3>
       <div class="sm:px-3 sm:pl-8 sm:pr-4">
         <p class="paragraph-mobile-small desktop:paragraph-regular">
-          Designing D&D Rulebook from start to finish was a thoroughly
-          enjoyable, but not always an easy process. It taught me a lot about the
-          process of user-centered design, about the value of proper research
-          and user testing, about listening to one's guts, and not being afraid
-          to change something if it results in a better product.
+          Even though I’ve designed and developed my own website, I count this project as my first full-blown website design, with lots of moving parts. I enjoyed the process, and I think I was successful in redesigning GABO Online for the better (thankfully, my usability test participants expressed the same view). I’m sure I missed a bunch of things I never thought about, and will only remember them once I publish this case study for everyone to see, but that’s okay.
         </p>
-        <p class="paragraph-mobile-small desktop:paragraph-regular">Research is something I would definitely do differently, more thoroughly now, using more techniques, not just user interviews. I would also start designing with atomic design in mind — this is something I didn't know much about when I began working on this project, but now I see that it would have made things so much easier and cleaner.</p>
+        <p class="paragraph-mobile-small desktop:paragraph-regular">I’ve learned that website design is an exhaustingly complex job, these 4.5 months were a true marathon, especially as a team of one. But it was also enjoyable, and I’ve learned that sometimes it’s good to take a step back and try something again after a few days had passed, that way a solution might be instantly obvious.</p>
       </div>
     </div>
 
-    <div class="desktop:col-start-7 desktop:col-end-13 col-span-full text-left">
-      <h3 class="h3-bold text-gray-900 mb-7">Next Steps</h3>
-      <div class="sm:px-3 sm:pl-8 sm:pr-4">
-        <p class="paragraph-mobile-small desktop:paragraph-regular">
-          I’m not done with this project yet. It still needs a lot of work and a lot of testing until I am willing to say it’s finished (let's be honest, no project is ever really finished). And while I cannot really provide numbers on business impact, I am confident that should I have the necessary resources to make D&D Rulebook more than just a concept, it would solve a lot of problems for D&D players.
-        </p>
-      </div>
-    </div>
+    
   </div>
 
 <WorkTogether/>
