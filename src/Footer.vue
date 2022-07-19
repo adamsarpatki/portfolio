9d04dfdcffd64cd5e3b060e3df07bc1fba71ab7d
Ádám Sárpátki <sarpatki.adam@gmail.com>
2022-07-19 19:57:01 +0200

    add new footer styling

--- src/Footer.vue
+++ src/Footer.vue
@@ -4,7 +4,7 @@
     <div class="w-full flex flex-col">
       <div
         class="
-          bg-blue-800
+          bg-white
           h-auto
           items-center
           flex-col
@@ -16,33 +16,34 @@
         "
       >
       <div class="desktop:px-400 xl:px-64 md:px-166 sm:px-124 px-6 mb-8 mt-6 max-w-1184">
-       <p class="h4-medium text-gray-50 mb-1">Ádám Sárpátki</p>
-       <p class="footer-regular text-gray-50">An enthusiastic UX designer based in Budapest, who loves combining his attention to detail with his creativity.</p>
+       <p class="h4-medium mb-1">Ádám Sárpátki</p>
+       <p class="footer-regular ">An enthusiastic UX designer based in Budapest, who wants to solve real problems in a friendly, diverse and inclusive environment, with incredible people.
+</p>
       </div>
 
        <div class="flex-row flex mb-6">
 
          <a href="https://www.linkedin.com/in/adamsarpatki/" target="_blank">
-          <vue-feather type="linkedin" class="m-2 fill-current text-gray-50 hover:fill-current hover:text-yellow-400"></vue-feather>
+          <vue-feather type="linkedin" class="m-2 text-teal-500 fill-current  hover:fill-current hover:text-yellow-300"></vue-feather>
          </a>
 
                   <a href="https://dribbble.com/adamsarpatki" target="_blank">
-          <vue-feather type="dribbble" class="m-2 fill-current text-gray-50 hover:fill-current hover:text-yellow-400"></vue-feather>
+          <vue-feather type="dribbble" class="m-2 text-teal-500 fill-current  hover:fill-current hover:text-yellow-300"></vue-feather>
          </a>
 
                   <a href="https://www.instagram.com/adam.sarpatki/" target="_blank">
-          <vue-feather type="instagram" class="m-2 fill-current text-gray-50 hover:fill-current hover:text-yellow-400"></vue-feather>
+          <vue-feather type="instagram" class="m-2 text-teal-500 fill-current  hover:fill-current hover:text-yellow-300"></vue-feather>
          </a>
 
                   <a href="https://twitter.com/adamsarpatki" target="_blank">
-          <vue-feather type="twitter" class="m-2 fill-current text-gray-50 hover:fill-current hover:text-yellow-400"></vue-feather>
+          <vue-feather type="twitter" class="m-2 text-teal-500 fill-current  hover:fill-current hover:text-yellow-300"></vue-feather>
          </a>
        </div>
 
       <div>
-        <p class="text-gray-50 footer-regular mb-2">Designed and developed by Ádám Sárpátki.</p>
+        <p class=" footer-regular mb-2">Designed and developed by Ádám Sárpátki.</p>
         <div class="mb-2"><router-link class="footer-link" to="/privacy">Privacy Policy & Copyright Info</router-link></div>
-        <p class="text-gray-50 footer-regular mb-6">© 2022 Ádám Sárpátki</p>
+        <p class=" footer-regular mb-6">© 2022 Ádám Sárpátki</p>
       </div>
 
       </div>
