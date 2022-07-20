fdd914e909376a8a761dccd18349b9e4b6ff68b5
Ádám Sárpátki <sarpatki.adam@gmail.com>
2022-07-20 09:34:44 +0200

    fix image borders

--- src/CaseStudies.vue
+++ src/CaseStudies.vue
@@ -8,7 +8,7 @@
 
 <div class="col-start-1 col-end-7 h-auto">
 
-  <div class="border-2 border-gray-900 sm:relative flex flex-col order-last sm:order-none w-auto mx-auto">
+  <div class="sm:relative flex flex-col order-last sm:order-none w-auto mx-auto">
       <picture>
         <source
           media="(max-width: 639px)"
@@ -23,7 +23,7 @@
           srcset="/src/assets/study-card-gabo-tablet@2x.png"
         />
         <img
-          class="h-438"
+          class="h-438 border-2 border-gray-900 rounded-xl"
           srcset="/src/assets/rulebook-redesigned-study-card.jpg"
           alt="The header image of the GABO Redesign"
         />
@@ -34,7 +34,7 @@
 
 <div class="col-start-7 col-end-13 h-auto">
 
-<div class="border-2 border-gray-900 sm:relative flex flex-col order-last sm:order-none w-auto mx-auto">
+<div class="sm:relative flex flex-col order-last sm:order-none w-auto mx-auto">
       <picture>
         <source
           media="(max-width: 639px)"
@@ -49,7 +49,7 @@
           srcset="/src/assets/study-card-gabo-tablet@2x.png"
         />
         <img
-          class="h-438 "
+          class="h-438 border-2 border-gray-900 rounded-xl"
           srcset="/src/assets/gabo-redesigned-study-card.jpg"
           alt="The header image of the GABO Redesign"
         />
--- src/Dribbble.vue
+++ src/Dribbble.vue
@@ -5,19 +5,19 @@
 
   <div class="study-grid mb-72">
 
-    <div class="border-2 border-gray-900  col-start-1 col-end-5 h-auto">
+    <div class="col-start-1 col-end-5 h-auto">
       <a href="https://dribbble.com/shots/18159083-Landing-Page-Daily-UI-003" target="_blank">
-      <img class="" src="/src/assets/dailyUI_day003_dribbble.png" alt="An image of Adam Sarpatki standing in front of a white wall and smiling."></a>
+      <img class="border-2 border-gray-900 rounded-xl" src="/src/assets/dailyUI_day003_dribbble.png" alt="An image of a website design, for an imaginary research agency, lead by librarians."></a>
     </div>
 
-    <div class="border-2 border-gray-900  col-start-5 col-end-9 h-auto">
+    <div class="col-start-5 col-end-9 h-auto">
       <a href="https://dribbble.com/shots/16694393-Credit-Card-Checkout-Daily-UI-002" target="_blank">
-      <img class="" src="/src/assets/dailyUI_day002.png" alt="An image of Adam Sarpatki standing in front of a white wall and smiling."></a>
+      <img class="border-2 border-gray-900 rounded-xl" src="/src/assets/dailyUI_day002.png" alt="An image of a mobile checkout flow design, for a yarn shop."></a>
     </div>
 
-    <div class="border-2 border-gray-900  col-start-9 col-end-13 h-auto">
+    <div class="col-start-9 col-end-13 h-auto">
       <a href="https://dribbble.com/shots/16685514-Sign-Up-Daily-UI-001" target="_blank">
-      <img class="" src="/src/assets/dailyUI_day001.png" alt="An image of Adam Sarpatki standing in front of a white wall and smiling."></a>
+      <img class="border-2 border-gray-900 rounded-xl" src="/src/assets/dailyUI_day001.png" alt="An image of an imaginary social media site for plant and gardening enthusiasts."></a>
     </div>
 
   </div>
