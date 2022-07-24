c443295fd6a14df376a84ca3a539830df5d3cbde
Ádám Sárpátki <sarpatki.adam@gmail.com>
2022-07-24 12:58:11 +0200

    add link to case study overlay

--- src/CaseStudies.vue
+++ src/CaseStudies.vue
@@ -32,12 +32,11 @@
 
         <!-- Overlay -->
         
-        <div
+        <router-link to="/gabo"><div
             class="xl:absolute xl:bottom-0 xl:left-0 w-full xl:h-0 flex flex-col justify-center xl:items-center items-start xl:bg-teal-900 bg-teal-400 xl:opacity-0 xl:group-hover:h-full xl:group-hover:opacity-90 xl:duration-500 xl:rounded-xl xl:px-16 rounded-b-xl xl:border-none border-b-2 border-x-2 border-gray-900 px-6  xl:mb-0 mb-4">
             <h3 class="h3-bold xl:text-white text-gray-900 xl:mb-4 mt-4 xl:mt-0 text-left xl:text-center">GABO Online Redesign</h3>
             <p class="subheading-italic xl:text-white text-gray-900 mb-4 text-left xl:text-center">Redesigning a Hungarian publisher's website and online shop.</p>
-            <router-link to="/gabo"><p class="xl:text-white text-gray-900 subheading-bold text-link decoration-teal-300 xl:mb-0 mb-4">Read the Case Study</p></router-link>
-        </div>
+        </div></router-link>
       </picture>
   </div>
 
@@ -67,13 +66,12 @@
 
         <!-- Overlay -->
 
-        <div
+        <router-link to="/rulebook"><div
             class="xl:absolute xl:bottom-0 xl:left-0 w-full xl:h-0 flex flex-col justify-center xl:items-center items-start xl:bg-teal-900 bg-yellow-300 xl:opacity-0 xl:group-hover:h-full xl:group-hover:opacity-90 xl:duration-500 xl:rounded-xl xl:px-16 rounded-b-xl xl:border-none border-b-2 border-x-2 border-gray-900 px-6  ">
             <!-- Add xl:mb-0 mb-4 before adding next card -->
             <h3 class="h3-bold xl:text-white text-gray-900 xl:mb-4 mt-4 xl:mt-0">D&D Rulebook</h3>
             <p class="subheading-italic xl:text-white text-gray-900 mb-4 text-left xl:text-center">A mobile-based solution for the players of Dungeons & Dragons.</p>
-            <router-link to="/rulebook"><p class="xl:text-white text-gray-900 subheading-bold text-link xl:mb-0 mb-4">Read the Case Study</p></router-link>
-        </div>
+        </div></router-link>
       </picture>
       
   </div>
